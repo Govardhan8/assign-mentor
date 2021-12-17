a4fa748b3834b189c3eb8f6e066b0d01f0314963
Govardhan8 <govardhanmurthy007@gmail.com>
2021-12-17 15:03:25 +0530

    reassigning students logic

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,6 @@ import { client } from './index.js'
 import { ObjectId } from 'mongodb'
 
 //Utility functions
-
 async function getUnassignedStudents() {
 	const result = await client
 		.db('myDB')
@@ -10,45 +9,6 @@ async function getUnassignedStudents() {
 		.find({ mentorID: null })
 		.toArray()
 	return result
-}
-
-async function addMentor(body) {
-	return await client.db('myDB').collection('mentor').insertOne(body)
-}
-
-async function addStudent(body) {
-	return await client.db('myDB').collection('student').insertOne(body)
-}
-
-async function assignMentor(mentor, student) {
-	student = student.map((student) => {
-		return ObjectId(student)
-	})
-
-	const mentorData = await client
-		.db('myDB')
-		.collection('mentor')
-		.updateOne(
-			{ _id: ObjectId(mentor) },
-			{
-				$addToSet: {
-					studentsID: {
-						$each: student,
-					},
-				},
-			}
-		)
-	const studentData = await client
-		.db('myDB')
-		.collection('student')
-		.updateMany(
-			{ _id: { $in: student } },
-			{ $set: { mentorID: ObjectId(mentor) } }
-		)
-	return {
-		studentData,
-		mentorData,
-	}
 }
 
 async function getStudentByMentorId(id) {
@@ -63,6 +23,71 @@ async function getStudentByMentorId(id) {
 		.find({ _id: { $in: studentData.studentsID } })
 		.toArray()
 	return result
+}
+
+async function addMentor(body) {
+	return await client.db('myDB').collection('mentor').insertOne(body)
+}
+
+async function addStudent(body) {
+	return await client.db('myDB').collection('student').insertOne(body)
+}
+
+async function assignMentor(mentor, students) {
+	students = students.map((student) => {
+		return ObjectId(student)
+	})
+
+	await unassignAssignedStudents(students)
+
+	const mentorData = await client
+		.db('myDB')
+		.collection('mentor')
+		.updateOne(
+			{ _id: ObjectId(mentor) },
+			{
+				$addToSet: {
+					studentsID: {
+						$each: students,
+					},
+				},
+			}
+		)
+	const studentData = await client
+		.db('myDB')
+		.collection('student')
+		.updateMany(
+			{ _id: { $in: students } },
+			{ $set: { mentorID: ObjectId(mentor) } }
+		)
+
+	return {
+		studentData,
+		mentorData,
+	}
+}
+
+async function unassignAssignedStudents(students) {
+	const studentList = await client
+		.db('myDB')
+		.collection('student')
+		.find({ _id: { $in: students } })
+		.toArray()
+	studentList.forEach(async (student) => {
+		if (student.mentorID) {
+			const result = await client
+				.db('myDB')
+				.collection('mentor')
+				.updateOne(
+					{ _id: student.mentorID },
+					{
+						$pull: {
+							studentsID: student._id,
+						},
+					}
+				)
+		}
+	})
 }
 
 export {
